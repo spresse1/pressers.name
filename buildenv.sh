4a90af7265684a1140f39667ebdc410c0d46dcdd
Steven Presser <steve@pressers.name>
2016-10-09 13:40:32 -0400

    Make updates a little more precise (by only removing necessary files) and make it so prod sites only get updated upon changes.

--- buildenv.sh
+++ buildenv.sh
@@ -6,7 +6,6 @@ set -e
 GITREPO=$(readlink -e $(dirname $0)) # The repo base so we can pull files as needed
 DESTINATION=${1:-.}
 SETTINGS=$(readlink -e ${2:-$GITREPO/settings-prod})
-DEST_EXISTS=$(test -e "$DESTINATION"; echo $?)
 # We do not take a variable settings template, as that would lead to a risk
 # of out-of-sync settings for test & deploy envronments
 
@@ -18,8 +17,13 @@ apt-get install -qq -y libtiff5-dev libjpeg62-turbo-dev zlib1g-dev \
 	libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev \
 	python-tk libmysqlclient-dev
 
-# Change to our destination
+# clean up critical files so we can "recreate" the environment without
+# actually wiping everything (and incurring an expensive pip download)
+rm -rf "$DESTINATION"/pressers_name
+rm -rf "$DESTINATION"/manage.py
 mkdir -p "$DESTINATION"
+
+# Change to destination
 cd "$DESTINATION"
 
 virtualenv -q .
@@ -27,19 +31,13 @@ virtualenv -q .
 
 pip install -q --upgrade -r "$GITREPO"/requirements.txt
 
-# Add the django shell, if not there already
-if [ ! -e "$DESTINATION"/manage.py ]
-then
-	bin/django-admin.py startproject pressers_name .
-fi
+# Re-init project
+bin/django-admin.py startproject pressers_name .
 
 # And copy in the static data
 rsync -qavz "$GITREPO/"payload/* "$DESTINATION"
 
-if [ $DEST_EXISTS != 0 ]
-then
-	cat "$SETTINGS" >> ./pressers_name/settings.py
-fi
+cat "$SETTINGS" >> ./pressers_name/settings.py
 
 # Do not fail if timezone info fails to load
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -p`cat ~/mysql-root-pass` mysql | grep -v "Warning: Unable to load" || :
