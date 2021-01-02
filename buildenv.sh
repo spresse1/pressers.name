28249500a9af49f0775df1853871451265f3c316
spresse1 <steve@pressers.name>
2021-01-02 17:03:14 -0500

    Add a version-independent path to python (so we don't have to make manual changes on future upgrades)

--- buildenv.sh
+++ buildenv.sh
@@ -28,6 +28,10 @@ cd "$DESTINATION"
 
 #virtualenv -q .
 python3 -m venv .
+
+PYPATH=$(ls -d lib/python* | sort | tail -n 1)
+ln -s "$(basename $PYPATH)" "lib/python"
+
 . bin/activate
 
 pip install -q --upgrade -r "$GITREPO"/requirements.txt
